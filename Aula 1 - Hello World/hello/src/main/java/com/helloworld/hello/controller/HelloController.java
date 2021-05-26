10226bd30bd63b888687dc7afc1e38d0a060b136
Gabriel Cerdeira <gabriel.cerdeira@outlook.com>
2021-05-25 23:05:23 -0300

    Adicionando descrição da atividade

--- Aula 1 - Hello World/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
+++ Aula 1 - Hello World/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -1,5 +1,17 @@
 package com.helloworld.hello.controller;
+/*Atividade 1 Spring-boot (minha primeira aplicação):
+Crie uma aplicação spring de hello world.
 
+A aplicação deverá conter um end-point que retorna
+uma string com as habilidades e mentalidades que você
+utilizou para realizar essa atividade.
+
+Atividade 2 Spring-boot (minha segunda aplicação):
+Crie uma aplicação spring de hello world.
+
+A aplicação deverá conter um end-point que retorna
+uma string com os seus objetivos de aprendizagem para
+essa semana.*/
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
