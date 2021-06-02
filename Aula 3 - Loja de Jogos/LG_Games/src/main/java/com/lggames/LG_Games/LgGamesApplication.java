03617e3ab14ecabf176010280f07cb79d5693902
Gabriel <gabriel.cerdeira@outlook.com>
2021-06-01 23:23:27 -0300

    Feat. Loja de Jogos

---
+++ Aula 3 - Loja de Jogos/LG_Games/src/main/java/com/lggames/LG_Games/LgGamesApplication.java
@@ -0,0 +1,13 @@
+package com.lggames.LG_Games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgGamesApplication.class, args);
+	}
+
+}
---
+++ Aula 3 - Loja de Jogos/LG_Games/src/test/java/com/lggames/LG_Games/LgGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lggames.LG_Games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
